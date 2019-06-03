c7a78daa2430b4456e5be9fef500d19f68312be8
Nick McGettigan <Nick.McGettigan@gmail.com>
2019-06-02 22:46:28 -0700

    Name Carryover between pages for chat.

--- serv.js
+++ serv.js
@@ -4,25 +4,29 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const socket = require('socket.io');
+
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
 const io = socket(app.listen(8080));
 
-io.on('connection', function(objectSocket) {
-    console.log("connection");
-    
 
+// Chat
+io.on('connection', function(objectSocket) { 
+    console.log("connection"); //testing
 });
 
 
+// Routing
 router.get('/login', (req, res) => {
     res.status(200);
     res.sendFile("/public/login.html", { root: '.' });
 });
 
 router.post('/game', (req, res) => {
-    res.status(200);
-    res.sendFile("/public/game.html", { root: '.' });
+        res.status(200);
+        res.sendFile("/public/game.html", { root: '.' });
 });
 
 router.use((req, res) => {
@@ -33,5 +37,5 @@ router.use((req, res) => {
 
 app.use('/', router);
 
-console.log('Serving on port: 8080');
+console.log('Serving on: http://localhost:8080/login');
 //app.listen(process.env.PORT || 8080);
