fcc568bbc0f96eb13a335abe9c4b230a79ef20c6
Nick McGettigan <Nick.McGettigan@gmail.com>
2019-06-06 13:24:29 -0700

    Full chat functionality: add emit events for client/server.  Add new locations for gmaps api.  Add beginning of hint api usage from wikipedia.

--- serv.js
+++ serv.js
@@ -3,7 +3,8 @@
 const express = require('express'); 
 const app = express();
 const router = express.Router();
-let socket = require('socket.io');
+const socket = require('socket.io');
+const request = require('request');
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -47,6 +48,12 @@ io.sockets.on('connection', function(objectSocket) {
 });
 
 
+// Hint Info
+//https://en.wikipedia.org/w/api.php?action=opensearch&search=arc_de_triomphe&limit=1&format=json
+request('https://en.wikipedia.org/w/api.php?action=opensearch&search=big_ben&limit=1&format=json', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(res.body[2]);
+});
 
 
 // Routing
@@ -78,11 +85,15 @@ let BB_URL = 'https://maps.googleapis.com/maps/api/streetview?size=600x450&locat
 let WM_URL = 'https://maps.googleapis.com/maps/api/streetview?size=600x450&location=38.8892703,-77.0393308&fov=40&heading=90&pitch=20&key=' + key;
 let CH_URL = 'https://maps.googleapis.com/maps/api/streetview?size=600x450&location=38.8896705,-77.0124246&fov=40&heading=90&pitch=10&key=' + key;
 let PSU_URL = 'https://maps.googleapis.com/maps/api/streetview?size=600x450&location=45.509355,-122.6815488&fov=90&heading=120&pitch=20&key=' + key;
+let AT_URL = 'https://maps.googleapis.com/maps/api/streetview?size=600x450&location=48.8739843,2.2944872&fov=100&heading=100&pitch=30&key=' + key;
+let SOH_URL = 'https://maps.googleapis.com/maps/api/streetview?size=600x450&location=-33.859922,151.2171313&fov=40&heading=335&pitch=2&key=' + key;
 
 /* Save Locations:
  * Big Ben
  * https://maps.googleapis.com/maps/api/streetview?size=300x300&location=51.5009153,-0.1247454&fov=90&heading=170&pitch=40&key=
- *
+ * Hint Info : https://en.wikipedia.org/w/api.php?action=opensearch&search=big_ben&limit=1&format=json
+ * 
+ * 
  * Washington Monument
  * 38.8894309,-77.0395274
  * https://maps.googleapis.com/maps/api/streetview?size=600x450&location=38.8892703,-77.0393308&fov=40&heading=90&pitch=20&key=
@@ -94,10 +105,20 @@ let PSU_URL = 'https://maps.googleapis.com/maps/api/streetview?size=600x450&loca
  * PSU
  * 45.509355,-122.6815488
  * https://maps.googleapis.com/maps/api/streetview?size=600x450&location=45.509355,-122.6815488&fov=90&heading=120&pitch=20&key=
+ * 
+ * Arc de Triomphe
+ * 48.8739843,2.2944872
+ * https://maps.googleapis.com/maps/api/streetview?size=600x450&location=48.8739843,2.2944872&fov=100&heading=100&pitch=30&key=
+ * 
+ * Sydney Opera House
+ * -33.859922,151.2171313
+ * https://maps.googleapis.com/maps/api/streetview?size=600x450&location=-33.859922,151.2171313&fov=40&heading=335&pitch=2&key=
+ * 
  */
 
 
 // Utilities
+
 function fileread(filename){
 
     var contents= fs.readFileSync(filename);
